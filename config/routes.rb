5498ed5bcb6afb45baa4152142844e155278f4f1
Alexandria McCarthy <alexandria.mccarthy77@gmail.com>
2019-04-15 16:05:27 -0700

    added TripsController

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'trips/show'
+  get 'trips/edit'
+  get 'trips/update'
+  get 'trips/create'
+  get 'trips/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
