db49e916ead294e20d153f9ab4564b5451f5dd2c
Alexandria McCarthy <alexandria.mccarthy77@gmail.com>
2019-04-16 16:34:58 -0700

    added method for determining total amount charged

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,3 +1,15 @@
 class Passenger < ApplicationRecord
-    has_many :trips
+  has_many :trips
+
+  def total_amount_charged
+    passenger_trips = self.trips.all
+
+    total_cost = 0
+
+    passenger_trips.each do |trip|
+      total_cost += trip.cost
+    end
+
+    return (total_cost.to_i / 100.0).round(2)
+  end
 end
