7017948c64126060979ccce10ba57eb8281aa5a9
Kate Sandler <kate@katekatekate.net>
2019-04-16 16:39:49 -0700

    Adds average_rating for driver

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,3 +1,17 @@
 class Driver < ApplicationRecord
-    has_many :trips
+  has_many :trips
+  validates :name, :vin, presence: true
+
+  def average_rating
+    rated_trips = 0
+    total_rating = 0.0
+
+    trips.each do |trip|
+      if trip.rating?
+        total_rating += trip.rating
+        rated_trips += 1
+      end
+      return (total_rating / rated_trips).to_f
+    end
+  end
 end
