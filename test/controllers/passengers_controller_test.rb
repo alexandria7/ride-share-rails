c963cc90acee8298b42065ed8d69b7c62bdd6d97
Alexandria McCarthy <alexandria.mccarthy77@gmail.com>
2019-04-16 15:01:30 -0700

    added corresponding tests for index and show for passengers

--- test/controllers/passengers_controller_test.rb
+++ test/controllers/passengers_controller_test.rb
@@ -1,12 +1,30 @@
 require "test_helper"
 
 describe PassengersController do
+  let (:passenger) {
+    Passenger.create name: "sample passenger", phone_num: "415-555-1001"
+  }
+
   describe "index" do
-    # Your tests go here
+    it "can get to the index path" do
+      get passengers_path
+
+      must_respond_with :success
+    end 
   end
 
   describe "show" do
-    # Your tests go here
+    it "can get a valid passenger" do
+      get passenger_path(passenger.id)
+
+      must_respond_with :success
+    end
+
+    it "will redirect for an invalid passenger" do
+      get passenger_path(-1)
+
+      must_respond_with :redirect
+    end
   end
 
   describe "edit" do
